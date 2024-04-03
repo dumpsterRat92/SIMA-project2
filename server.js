dedcfd41a34d1d293678d59c3b12c1b9760cf763
Your Name <you@example.com>
2024-04-03 16:58:55 -0700

    Added functions to server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+
+const sequelize = require('./config');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = 
