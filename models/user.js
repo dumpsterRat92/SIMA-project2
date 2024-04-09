1d831c80b52a6786ac97be8c16b4d78ae3084d0d
VZachary13 <vinicius.zachary@gmail.com>
2024-04-09 10:12:19 -0700

    added bcrypt to user.js for real this time

--- models/user.js
+++ models/user.js
@@ -37,6 +37,12 @@ User.init(
     },
   },
   {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
