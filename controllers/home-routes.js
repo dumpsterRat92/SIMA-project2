3bf689124923bd2bdf9913053f820004c8290717
unknown <jongarcia.realty@gmail.com>
2024-04-10 16:16:14 -0700

    added functionality to gametags

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,18 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
+const Gametype = require('../models/Gametype');
 
 
 router.get('/', withAuth, async (req, res) => {
     try {
+        const gametype = await Gametype.findAll();
+        const gametypes = gametype.map((gametype) => 
+        gametype.get({ plain: true })
+        )
+
+        console.log('gameroutes leg')
         res.render('home', {
+            gametypes,
             loggedIn: req.session.loggedIn,
         });
     } catch (err) {
